b5cfd3bbbf3fe44006cdcc706cb008ce5ad787ba
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-03-12 15:00:26 +0000

    added method with recursive method where if 0 returns 0 if 1 returns 0 and else does the calculation based on the number inputed

--- Special/Program.cs
+++ Special/Program.cs
@@ -10,9 +10,17 @@ namespace Special
             Console.Write("Insert n-th number of the special sequence: ");
             int n = int.Parse(Console.ReadLine());
 
-            
         }
 
-
+        private static int GetSpecial(int n)
+        {
+            if (n==0)
+                return 0;
+            else if (n==1)
+                return 1;
+            else    
+                return GetSpecial(n-1) + 2 * GetSpecial(n-2);
+            
+        }
     }
 }
