c7dd8e2a4e5955ea3d80b64068f8ae47d13ca59f
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-03-12 13:41:35 +0000

    Added code for string conversion

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class BetterDecorador
+{
+    private static string Decor(string s, char dec, int count)
+    {
+        string decoration = new string(dec, count);
+        return $"{decoration} {s} {decoration}";
+    }
+
+ 
