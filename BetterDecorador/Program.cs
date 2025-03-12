2c888fba93487fa79a75f145d787d7e702791180
Joao-oliveiraribeiro <115244204+Joao-oliveiraribeiro@users.noreply.github.com>
2025-03-12 13:48:33 +0000

    Added documentation for Decor function

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
 class BetterDecorador
-{
+{   
+    /// <summary>
+    /// Get numbers convert into a string 
+    // and return a interpolated string
+    /// </summary>
+    /// <param name="s"> inputed string </param>
+    /// <param name="dec"> inputed character </param>
+    /// <param name="count"> inputed number of times of character </param>
+    /// <returns> Interpolated string with specified functions </returns> <summary>
     private static string Decor(string s, char dec, int count)
     {
         string decoration = new string(dec, count);
